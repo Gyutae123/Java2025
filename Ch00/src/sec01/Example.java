90bebecafdd55fba64061ffde9b72ffe9a4c6500
Gyutae123 <kkssaln@naver.com>
2025-09-09 10:08:04 +0900

    123

---
+++ Ch00/src/sec01/Example.java
@@ -0,0 +1,14 @@
+package sec01;
+
+public class Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 1;
+		int b = 2;
+		int result = a + b;
+		System.out.println(result);
+
+	}
+
+}
---
+++ Ch02/src/sec01/ScapeTest.java
@@ -0,0 +1,19 @@
+package sec01;
+
+public class ScapeTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		if (a > 1)
+		{
+			int b = a;
+		}
+		for (int i = 0; i < 10; i++)
+		{
+			int c = 1;
+		}
+
+	}
+
+}
