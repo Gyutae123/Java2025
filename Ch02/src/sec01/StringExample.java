3e8ac9163af142f1a7808664013aaae209c55d90
Gyutae123 <kkssaln@naver.com>
2025-09-09 10:27:32 +0900

    Create StringExample.java

---
+++ Ch02/src/sec01/StringExample.java
@@ -0,0 +1,16 @@
+package sec01;
+
+public class StringExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char var1 = 'A';
+		char var2 = "홍길동";
+		
+		String var3 = 'A';
+		String var4 = "홍길동";
+		
+
+	}
+
+}
