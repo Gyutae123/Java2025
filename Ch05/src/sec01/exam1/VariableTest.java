3e9f56595a01538cfa7784dc3ebd0b477af21302
Gyutae123 <kkssaln@naver.com>
2025-09-23 10:49:13 +0900

    0923수업실습

---
+++ Ch05/src/sec01/exam1/VariableTest.java
@@ -0,0 +1,20 @@
+package sec01.exam1;
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		int a;
+//		int b = a;
+		
+		String str1 = null;
+		String str2 = str1;
+		
+//		int[] intArray = null;
+//		intArray[0] = 10; 배열이 null지정으로 값을 넣을 칸이 없음
+		
+		
+		
+	}
+
+}
---
+++ Ch05/src/sec02/exam1/ArrayTest.java
@@ -0,0 +1,17 @@
+package sec02.exam1;
+
+public class ArrayTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] intArray = new int[10];
+		for(int i=0; i < 10; i++) {
+			System.out.println(intArray[i]); //배열에 값 입력을 안해서 0만 출력
+		}
+		
+		int[][] scores = new int [2][3];
+		scores[0] = new int[3];
+		
+	}
+
+}
