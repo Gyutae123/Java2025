5e1841451954efba6c2b41edfca96ae40bd012fa
Gyutae123 <kkssaln@naver.com>
2025-10-02 14:49:09 +0900

    1002수업실습

---
+++ Ch06/src/sec03/exam1/MainClass.java
@@ -0,0 +1,15 @@
+package sec03.exam1;
+
+public class MainClass {
+	public static void name(String[] args) {
+		Car c1 = new Car();
+		Car c2 = new Car("Hyundai");
+		Car c3 = new Car("Kia", "Sedan");
+		Car c4 = new Car("Kia", "truck", 150);
+		
+		c1.ShowInfo();
+		c2.ShowInfo();
+		c3.ShowInfo();
+		c4.ShowInfo();
+	}
+}
