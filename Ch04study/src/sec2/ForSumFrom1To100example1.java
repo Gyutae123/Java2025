a0d93b659676b1e5bc26fbc492446cadf234852a
Gyutae123 <kkssaln@naver.com>
2025-11-21 17:44:32 +0900

    1121개인공부

---
+++ Ch04study/src/sec2/ForSumFrom1To100example1.java
@@ -0,0 +1,15 @@
+package sec2;
+
+public class ForSumFrom1To100example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum = 0;
+		for(int i = 1; i <= 100; i++) {
+			sum += i;
+		}
+		
+		System.out.println("1~100의 합 : " + sum);
+	}
+
+}
