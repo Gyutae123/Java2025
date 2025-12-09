0da18397a67eaa25715b654644c1ce43dedf5cbc
Gyutae123 <kkssaln@naver.com>
2025-12-09 22:44:04 +0900

    1209개인공부

---
+++ Ch06study/src/sec02/FieldIntValueExample.java
@@ -0,0 +1,22 @@
+package sec02;
+
+public class FieldIntValueExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FieldIntValue fiv = new FieldIntValue();
+		
+		System.out.println(fiv.byteField);
+		System.out.println(fiv.shortField);
+		System.out.println(fiv.intField);
+		System.out.println(fiv.longField);
+		System.out.println(fiv.booleanField);
+		System.out.println(fiv.charField);
+		System.out.println(fiv.floatField);
+		System.out.println(fiv.duobleField);
+		System.out.println(fiv.arrField);
+		System.out.println(fiv.referenceField);
+
+	}
+
+}
